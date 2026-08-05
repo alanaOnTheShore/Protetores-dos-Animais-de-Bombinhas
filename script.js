b336310fcf5540770babea5d3e92d8e419b52252
Alana Nascimento <164276418+alanaOnTheShore@users.noreply.github.com>
2026-08-05 14:10:01 -0300

    Update script.js
    Adição de botão de voltar ao topo

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+// MENU HAMBURGUER
 let menuHamburger = document.getElementById("hamburguer");
 const menu = document.getElementById("menu");
 
 menuHamburger.addEventListener("click", function () {
   menu.classList.toggle("ativa");
+});
+
+//BOTAO VOLTAR AO TOPO
+const btn = document.getElementById("btn-voltar");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
+    btn.style.display = "flex";
+  } else {
+    btn.style.display = "none";
+  }
 });
